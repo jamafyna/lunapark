20eb024d5d1576846a932b58ee0b944aca3a9e25
Jana Novotna <janka.novot@seznam.cz>
2015-07-10 13:37:02 +0200

    pridani novych souboru .cs

---
+++ zapoctak-ProgramovaniII_ls2014/Path.cs
@@ -0,0 +1,62 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zapoctak_ProgramovaniII_ls2014
+{
+    class StonePath: Path
+    {
+        public override void Create(int x, int y){
+            throw new NotImplementedException();
+        }
+        
+         public override void Demolish(){
+            throw new NotImplementedException();
+        }
+        public override void Click(){
+            throw new NotImplementedException();
+        }
+    }
+    class AsphaltPath: Path{
+    public override void Create(int x, int y){
+            throw new NotImplementedException();
+        }
+         public override void Demolish(){
+            throw new NotImplementedException();
+        }
+        public override void Click(){
+            throw new NotImplementedException();
+        }
+    
+    }
+    class SandPath: Path{
+         public override void Create(int x, int y){
+            throw new NotImplementedException();
+        }
+         public override void Demolish(){
+            throw new NotImplementedException();
+        }
+        public override void Click(){
+            throw new NotImplementedException();
+        }
+    }
+    class MarblePath : Path {
+        public override void Create(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+        public override void Demolish()
+        {
+            throw new NotImplementedException();
+        }
+        public override void Click()
+        {
+            throw new NotImplementedException();
+        }
+    
+    }
+
+}
---
+++ zapoctak-ProgramovaniII_ls2014/View.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zapoctak_ProgramovaniII_ls2014
+{
+    class View
+    {
+        Model model;
+        public View(Model m) {
+            this.model = m;       
+        }
+        public void Action() {
+
+            throw new NotImplementedException();
+        }
+        private void DestructDirty() {
+            foreach (var item in model.dirtyDestruct)
+            {
+                item.Dispose();
+            }               
+        }
+        private void NewDirty() { 
+            
+        
+        }
+    }
+}
